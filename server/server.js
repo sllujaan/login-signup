e8298886adda0d937dc07cc575bb97ddcd41e567
salmanAltaf101 <sllujaan44@gmail.com>
2020-06-18 17:37:45 +0500

    modified yaml.config file

--- server/server.js
+++ server/server.js
@@ -200,6 +200,6 @@ function getUrlParameter(url, parameterName) {
 }
 
 
-
-app.listen(3000)
+const PORT = process.env.PORT || 3000
+app.listen(PORT)
 console.log(`listening to http://localhost:3000`)
--- server/test_server/serverDB.test.js
+++ server/test_server/serverDB.test.js
@@ -4,4 +4,6 @@ var database = require("../serverDB.js")
 
 test('adds 1 + 2 to equal 3', () => {
     expect(database.sum(1, 2)).toBe(3);
-  });
+});
+
+
